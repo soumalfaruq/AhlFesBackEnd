0144ea1e2c13d998b7b68b0c8793029ae4a4f392
soums <oumar.soumare@uvs.edu.sn>
2021-07-13 01:15:25 +0000

    Ajout commande et images

--- src/main/java/com/ahfes/api/controller/CommandeController.java
+++ src/main/java/com/ahfes/api/controller/CommandeController.java
@@ -5,14 +5,22 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ahfes.api.model.Commande;
 import com.ahfes.api.model.Food;
 import com.ahfes.api.model.FoodType;
+import com.ahfes.api.model.Product;
 import com.ahfes.api.model.ProductCategory;
+import com.ahfes.api.model.TypeProduct;
+import com.ahfes.api.repository.CommandeRepository;
 import com.ahfes.api.repository.FoodRepository;
 import com.ahfes.api.repository.FoodTypeRepository;
 import com.ahfes.api.repository.ProductCategoryRepositorty;
+import com.ahfes.api.repository.ProductRepository;
+import com.ahfes.api.repository.TypeProductRepository;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 @RestController
 @CrossOrigin("*")
@@ -20,19 +28,28 @@ public class CommandeController {
 
 	@Autowired
 	FoodRepository food;
-	
+	@Autowired
+	CommandeRepository commande;
 	@Autowired
 	FoodTypeRepository type;
 	@Autowired
-	ProductCategoryRepositorty pc;
+	TypeProductRepository pt;
+	@Autowired
+	ProductRepository p;
 	
 	@GetMapping(value = "/foodorder")
 	public List<Food> listFood() {
 		return food.findByOrderByNbrVente();
 	}
-	@GetMapping(value = "/productcategory")
-	public List<ProductCategory> listProductCategory() {
-		return pc.findAll();
+	
+	@GetMapping(value = "/product")
+	public List<Product> listProduct() {
+		return p.findAll();
+	}
+	
+	@GetMapping(value = "/productType")
+	public List<TypeProduct> listProductType() {
+		return pt.findAll();
 	}
 	
 	@GetMapping(value = "/typefood")
@@ -44,4 +61,14 @@ public class CommandeController {
 	public List<Food> foods() {
 		return food.findAll();
 	}
+	
+	 @PostMapping("/commander")
+	    void addUser(@RequestBody Commande c) {
+		 	Commande lastCommande=commande.findTopByOrderByIdDesc();
+		 	if(lastCommande.getId()!=null) {
+		 	c.setNumCommande("00"+lastCommande.getId()+1+"A");
+		 	}
+	        commande.save(c);
+	    }
+	
 }
--- src/main/java/com/ahfes/api/model/Product.java
+++ src/main/java/com/ahfes/api/model/Product.java
@@ -21,7 +21,7 @@ public class Product implements Serializable {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long Id;
+	private Long id;
 	private String name;
 	@ManyToOne
 	@JoinColumn(name = "sub_category_id")
@@ -42,6 +42,12 @@ public class Product implements Serializable {
 		this.quantity = quantity;
 		this.picture = picture;
 		this.numberSeller = numberSeller;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
 	}
 	public String getName() {
 		return name;
@@ -73,7 +79,6 @@ public class Product implements Serializable {
 	public void setNumberSeller(Integer numberSeller) {
 		this.numberSeller = numberSeller;
 	}
-	
 	
 	
 }
